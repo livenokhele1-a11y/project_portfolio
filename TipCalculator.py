3830daecfb62fb3eb752ef4154025b5a80ab37d6
Live Nokhele <livenokhele1@gmail.com>
2026-07-31 09:01:13 +0200

    Delete TipCalculator.py

--- TipCalculator.py
+++
@@ -1,7 +0,0 @@
-meal = input("How much was the meal? $")
-percent = input("What percentage would you like to tip? %")
-leave = float(meal)*float(percent)/100
-print("Leave $", f"{leave:.2f}", "as a tip")
-
-
-
